0f33be95314c3e8073af4bdd319d1a07360bec91
Aybuke Altuntas <aybukealtuntas@192.168.1.19>
2026-02-13 19:05:49 +0300

    fix(utils): resolve incorrect euclidean distance implementation

--- utils/distances.py
+++ utils/distances.py
@@ -1,3 +1,4 @@
+import math
 
 def manhattan(a,b):
     d_x = b[0] - a[0]
@@ -10,5 +11,5 @@ def euclidean(a, b):
     d_x = b[0] - a[0]
     d_y = b[1] - a[1]
 
-    distance = d_x**2 + d_y**2
-    return distance
+    distance = (d_x**2) + (d_y**2)
+    return math.sqrt(distance)
