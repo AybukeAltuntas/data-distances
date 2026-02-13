258b94a1e15bc0201454afa85ada9ab8a87b6661
Aybuke Altuntas <aybukealtuntas@192.168.1.19>
2026-02-13 19:00:06 +0300

    feat: add euclidean distance function

--- utils/distances.py
+++ utils/distances.py
@@ -4,3 +4,11 @@ def manhattan(a,b):
     d_y = b[1] - a[1]
     distance = abs(d_x) + abs(d_y)
     return distance
+
+
+def euclidean(a, b):
+    d_x = b[0] - a[0]
+    d_y = b[1] - a[1]
+
+    distance = d_x**2 + d_y**2
+    return distance
